9c89c42aee4812811cc49a3c214f1821aee0dc1c
Rutuparn Pawar <rutuparn.17U253@viit.ac.in>
2020-03-19 15:15:19 +0530

    File added

---
+++ Tic_Tac_Toe.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+REM Batch file to run Tic Tac Toe game 
+
+TITLE Tic Tac Toe 
+
+ECHO Tic Tac Toe
+ECHO Developed by Rutuparn Pawar (InputBlackBoxOutput)
+ECHO.
+
+ECHO Please minimize this window.
+ECHO.
+
+python Tic_Tac_Toe.py 
+
+PAUSE
+EXIT
